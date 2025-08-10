212bffd4a8ac4e6789424bc02a679d21b4ce5007
EmperorLoki <angeloki17.ey@gmail.com>
2025-08-10 17:33:49 +0800

    feat(MyObject): add MyObject with properties and method

---
+++ lib/object.dart
@@ -0,0 +1,12 @@
+class MyObject {
+  String name;
+  int value;
+
+  // Parameterized constructor
+  MyObject(this.name, this.value);
+
+  // Method to display object info
+  void displayInfo() {
+    print('Name: $name,');
+  }
+}
